ead3292a683fb861fe7f0d56d56164ee379b2866
krisnasaputtra <krisna.saputra031002@gmail.com>
2024-08-26 17:46:24 +0700

    feat: add footer & to top button

---
+++ src/app/$action/constants.ts
@@ -0,0 +1,32 @@
+export const LIST_TERMS = [
+  {
+    value: "Privasi",
+    url: "https://bri.co.id/privacy",
+    extern: true,
+  },
+  {
+    value: "Syarat & Ketentuan",
+    url: "https://bri.co.id/term-of-use",
+    extern: true,
+  },
+  {
+    value: "Sitemap",
+    url: "https://bri.co.id/sitemap",
+    extern: true,
+  },
+  {
+    value: "Karir",
+    url: "https://bri.co.id/web/erecruitment",
+    extern: true,
+  },
+  {
+    value: "CMS BRI",
+    url: "https://ibank.bri.co.id/cms/",
+    extern: true,
+  },
+  {
+    value: "E-form BRI",
+    url: "https://eform.bri.co.id/",
+    extern: true,
+  },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,301 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/lib/element/global/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: "12px",
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        mdmax: { max: '768px' },
+      },
+      fontSize: {
+        // heading s
+        "heading-s-bold": [
+          "30px",
+          {
+            lineHeight: "38px",
+            fontWeight: 700,
+          },
+        ],
+        "heading-s-semibold": [
+          "30px",
+          {
+            lineHeight: "38px",
+            fontWeight: 600,
+          },
+        ],
+        "heading-s-medium": [
+          "30px",
+          {
+            lineHeight: "38px",
+            fontWeight: 500,
+          },
+        ],
+        "heading-s-regular": [
+          "30px",
+          {
+            lineHeight: "38px",
+            fontWeight: 400,
+          },
+        ],
+        "heading-s-light": [
+          "30px",
+          {
+            lineHeight: "38px",
+            fontWeight: 300,
+          },
+        ],
+        // heading m
+        "heading-m-bold": [
+          "60px",
+          {
+            lineHeight: "72px",
+            fontWeight: 700,
+          },
+        ],
+        "heading-m-semibold": [
+          "60px",
+          {
+            lineHeight: "72px",
+            fontWeight: 600,
+          },
+        ],
+        "heading-m-medium": [
+          "60px",
+          {
+            lineHeight: "72px",
+            fontWeight: 500,
+          },
+        ],
+        "heading-m-regular": [
+          "60px",
+          {
+            lineHeight: "72px",
+            fontWeight: 400,
+          },
+        ],
+        "heading-m-light": [
+          "60px",
+          {
+            lineHeight: "72px",
+            fontWeight: 300,
+          },
+        ],
+        // text xxl
+        "xxl-bold": [
+          "20px",
+          {
+            lineHeight: "32px",
+            fontWeight: 700,
+          },
+        ],
+        "xxl-semibold": [
+          "20px",
+          {
+            lineHeight: "32px",
+            fontWeight: 600,
+          },
+        ],
+        "xxl-medium": [
+          "20px",
+          {
+            lineHeight: "32px",
+            fontWeight: 500,
+          },
+        ],
+        "xxl-regular": [
+          "20px",
+          {
+            lineHeight: "32px",
+            fontWeight: 400,
+          },
+        ],
+        "xxl-light": [
+          "20px",
+          {
+            lineHeight: "32px",
+            fontWeight: 300,
+          },
+        ],
+        // text xl
+        "xl-bold": [
+          "18px",
+          {
+            lineHeight: "28px",
+            fontWeight: 700,
+          },
+        ],
+        "xl-semibold": [
+          "18px",
+          {
+            lineHeight: "28px",
+            fontWeight: 600,
+          },
+        ],
+        "xl-medium": [
+          "18px",
+          {
+            lineHeight: "28px",
+            fontWeight: 500,
+          },
+        ],
+        "xl-regular": [
+          "18px",
+          {
+            lineHeight: "28px",
+            fontWeight: 400,
+          },
+        ],
+        "xl-light": [
+          "18px",
+          {
+            lineHeight: "28px",
+            fontWeight: 300,
+          },
+        ],
+        // text l
+        "l-bold": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: 700,
+          },
+        ],
+        "l-semibold": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: 600,
+          },
+        ],
+        "l-medium": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: 500,
+          },
+        ],
+        "l-regular": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: 400,
+          },
+        ],
+        "l-light": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: 300,
+          },
+        ],
+        // text m
+        "m-bold": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: 700,
+          },
+        ],
+        "m-semibold": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: 600,
+          },
+        ],
+        "m-medium": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: 500,
+          },
+        ],
+        "m-regular": [
+          "14px",
+          {
+            lineHeight: "24px",
+            fontWeight: 400,
+          },
+        ],
+        "m-light": [
+          "14px",
+          {
+            lineHeight: "24px",
+            fontWeight: 300,
+          },
+        ],
+        // text s
+        "s-bold": [
+          "12px",
+          {
+            lineHeight: "18px",
+            fontWeight: 700,
+          },
+        ],
+        "s-semibold": [
+          "12px",
+          {
+            lineHeight: "18px",
+            fontWeight: 600,
+          },
+        ],
+        "s-medium": [
+          "12px",
+          {
+            lineHeight: "18px",
+            fontWeight: 500,
+          },
+        ],
+        "s-regular": [
+          "12px",
+          {
+            lineHeight: "18px",
+            fontWeight: 400,
+          },
+        ],
+        "s-light": [
+          "12px",
+          {
+            lineHeight: "18px",
+            fontWeight: 300,
+          },
+        ],
+      },
+      colors: {
+        "blue-01": "#00549B",
+        "blue-02": "#1078CA",
+        "blue-03": "#DDEFFC",
+        "orange-01": "#F87304",
+        "orange-02": "#F0F8FF",
+        "orange-03": "#FDCAA0",
+        "orange-04": "#FEEDDF",
+        "white-01": "#FFFFFF",
+        "white-02": "#F5FBFF",
+        "gray-01": "#292929",
+        "gray-02": "#929393",
+        "gray-03": "#EAEBEB",
+        "gray-04": "#A3A3A3",
+        "light-02": "#EAEBEB",
+        "light-08": "#777777",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
