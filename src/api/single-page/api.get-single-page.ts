b4fb17e26f48395cc5574979d75a83652574921e
diko-mahendra <devteam@indesc.com>
2024-09-12 00:18:55 +0700

    feat: integration switching lang and button to top

--- src/api/single-page/api.get-single-page.ts
+++ src/api/single-page/api.get-single-page.ts
@@ -2,14 +2,17 @@
 
 import { get } from "@/api/common/fetch";
 
-export async function API_GetSinglePage({ }: // lang,
-  {
+export async function API_GetSinglePage({ lang }
+  :{
     lang: string;
   }): Promise<any> {
   try {
-    const response = await get("/node/15?_format=json_recursive");
+    const isEnglish = lang === "en" ? "" : "/id"
+    const response = await get(`${isEnglish}/node/15?_format=json_recursive`);
+    
     return response;
   } catch (error) {
+   
     // eslint-disable-next-line no-console
     console.error("An error occurred during Get Single Page:", error);
     return [];
--- src/app/aether/page.tsx
+++ src/app/aether/page.tsx
@@ -5,9 +5,16 @@ import { COMPONENT_MAP_WIDGET } from "./$constant";
 import { ACT_GetSinglePage } from "./$action/action.get.single-page";
 import { T_FieldComponent } from "@/api/single-page/api.get-single-page.type";
 import { T_Widget } from "./$constant/types";
+import { Locale } from "@/i18n-config";
+import ScrollToTop from "@/lib/element/global/scroll.top";
 
-export default async function PageAether() {
-  const data = await ACT_GetSinglePage({ lang: "en" });
+export default async function PageAether({
+  searchParams
+}: {
+  searchParams: { lang: Locale };
+}) {
+
+  const data = await ACT_GetSinglePage({ lang: searchParams?.lang });
 
   const components = data?.field_components
     ?.map((component: T_FieldComponent) => {
@@ -35,6 +42,7 @@ export default async function PageAether() {
           <Component {...props} />
         </React.Fragment>
       ))}
+      <ScrollToTop />
     </React.Fragment>
   );
 }
