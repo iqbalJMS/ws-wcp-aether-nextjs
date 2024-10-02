cc767a29182c34b11bc7c1f69d0e4984b0f75b28
diko-mahendra <devteam@indesc.com>
2024-10-02 11:58:28 +0700

    feat: replace node to alias

--- src/app/aether/$action/action.get.single-page.ts
+++ src/app/aether/$action/action.get.single-page.ts
@@ -4,11 +4,11 @@ import { API_GetSinglePage } from '@/api/single-page/api.get-single-page';
 
 export async function ACT_GetSinglePage({
   lang,
-  node,
+  alias,
 }: {
-  node: string;
+  alias: string;
   lang: string;
 }): Promise<any> {
-  const response = await API_GetSinglePage({ lang, node });
+  const response = await API_GetSinglePage({ lang, alias });
   return response;
 }
