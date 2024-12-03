9ab060a0e73e2f080c5bf3c754f42beddbfba653
Malik Sukmo Satrio <maliksatrio@gmail.com>
2024-12-03 17:10:16 +0700

    feat: :sparkles: update card management

--- src/app/(views)/$element/card/client.card.variant12.tsx
+++ src/app/(views)/$element/card/client.card.variant12.tsx
@@ -27,45 +27,74 @@ export function CE_CardVariant12({ data }: T_CardVariant12Props) {
               key={index}
               className="w-1/3 mdmax:w-full flex-none px-5 mb-10"
             >
-              <div className="bg-white  shadow-lg rounded-br-[5rem] overflow-hidden">
-                {item?.image && (
-                  <div className="w-full h-[20rem] ">
-                    <Image
-                      extern={false}
-                      src={item?.image}
-                      alt="image"
-                      width={1920}
-                      height={1080}
-                      className="w-full h-full object-cover"
-                    />
+              <div className="bg-white  shadow-lg rounded-br-[5rem] overflow-hidden relative group">
+                <div className="absolute top-full group-hover:top-0 transition-all ease-in-out duration-300 left-0 w-full h-full bg-blue-02 z-10 flex items-center justify-center">
+                  <div>
+                    {item?.title && (
+                      <div className="text-2xl font-medium text-center text-white  mb-2">
+                        {item?.title}
+                      </div>
+                    )}
+                    {item?.description && (
+                      <div className="mb-2 text-xl  text-center  text-white">
+                        {parseHTMLToReact(item?.description)}
+                      </div>
+                    )}
+                    {item?.button?.title && (
+                      <div className="text-center relative z-10">
+                        <Link
+                          href={item?.button?.link ?? ''}
+                          extern={item?.button?.extern}
+                          target={item?.button?.extern ? '_blank' : ''}
+                        >
+                          <div className="text-white text-xs inline-block ">
+                            {item?.button?.title}
+                          </div>
+                        </Link>
+                      </div>
+                    )}
                   </div>
-                )}
-                <div className="p-5 px-10 relative overflow-hidden">
-                  <div className="w-[15rem] h-[10rem] bg-blue-01 absolute -bottom-[6rem] -right-[4rem] transform rotate-[-30deg] z-0"></div>
+                </div>
+                <div className="relative z-0">
+                  {item?.image && (
+                    <div className="w-full h-[30rem] ">
+                      <Image
+                        extern={false}
+                        src={item?.image}
+                        alt="image"
+                        width={1920}
+                        height={1080}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  )}
+                  <div className="p-5 px-10 relative overflow-hidden">
+                    <div className="w-[15rem] h-[10rem] bg-blue-02 absolute -bottom-[6rem] -right-[4rem] transform rotate-[-30deg] z-0"></div>
 
-                  {item?.title && (
-                    <div className="text-xl font-medium text-center  mb-2">
-                      {item?.title}
-                    </div>
-                  )}
-                  {item?.description && (
-                    <div className="mb-20 text-base  text-center">
-                      {parseHTMLToReact(item?.description)}
-                    </div>
-                  )}
-                  {item?.button?.title && (
-                    <div className="text-right relative z-10">
-                      <Link
-                        href={item?.button?.link ?? ''}
-                        extern={item?.button?.extern}
-                        target={item?.button?.extern ? '_blank' : ''}
-                      >
-                        <div className="text-white text-xs inline-block transform rotate-[-30deg] pl-5">
-                          {item?.button?.title}
-                        </div>
-                      </Link>
-                    </div>
-                  )}
+                    {item?.title && (
+                      <div className="text-xl font-medium text-center  mb-2">
+                        {item?.title}
+                      </div>
+                    )}
+                    {item?.description && (
+                      <div className="mb-20 text-base  text-center">
+                        {parseHTMLToReact(item?.description)}
+                      </div>
+                    )}
+                    {item?.button?.title && (
+                      <div className="text-right relative z-10">
+                        <Link
+                          href={item?.button?.link ?? ''}
+                          extern={item?.button?.extern}
+                          target={item?.button?.extern ? '_blank' : ''}
+                        >
+                          <div className="text-white text-xs inline-block transform rotate-[-30deg] pl-5">
+                            {item?.button?.title}
+                          </div>
+                        </Link>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
