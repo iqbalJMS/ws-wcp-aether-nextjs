d0f591d3c513e113fd058481426e7fa00579f724
krisnasaputtra <krisna.saputra031002@gmail.com>
2025-01-15 09:57:51 +0700

    feat: add promo news

--- src/app/(views)/$constant/types/widget/content_type.ts
+++ src/app/(views)/$constant/types/widget/content_type.ts
@@ -1,3 +1,41 @@
+type T_FieldMediaImage = {
+  entity_type: any[];
+  entity_bundle: any[];
+  fid: any[];
+  uuid: any[];
+  langcode: any[];
+  uid: any[];
+  filename: any[];
+  uri: Array<{ value: string; url: string }>;
+  filemime: any[];
+  filesize: any[];
+  status: any[];
+  created: any[];
+  changed: any[];
+};
+
+type T_FieldImage = {
+  entity_type: any[];
+  entity_bundle: any[];
+  mid: any[];
+  uuid: any[];
+  vid: any[];
+  langcode: any[];
+  bundle: any[];
+  revision_created: any[];
+  revision_user: any[];
+  status: any[];
+  uid: any[];
+  name: any[];
+  thumbnail: Array<T_FieldMediaImage>;
+  created: any[];
+  changed: any[];
+  default_langcode: any[];
+  revision_translation_affected: any[];
+  path: Array<{ alias: any }>;
+  field_media_image: any[];
+};
+
 export type T_Content_Type = {
   nid: Array<{ value: string }>;
   uuid: Array<{ value: string }>;
@@ -9,6 +47,8 @@ export type T_Content_Type = {
   created: Array<{ value: string }>;
   path: Array<{ alias: string }>;
   field_plain_description: Array<{ value: string }>;
+  field_image: Array<T_FieldImage>;
+  body: Array<{ value: string }>;
 };
 
 export type T_News = {
