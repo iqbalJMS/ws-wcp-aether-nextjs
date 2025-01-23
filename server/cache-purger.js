20e87829397cbad43154b03724803439330dc249
Muhammad Iqbal <iqbal@indesc.com>
2025-01-23 14:47:19 +0700

    update server dir

--- server/cache-purger.js
+++ server/cache-purger.js
@@ -2,16 +2,15 @@
 
 const fs = require('fs');
 const path = require('path');
-const cachePath = path.join(process.cwd(), '.next/cache/fetch-cache');
 
-// one hour inteval
-const cachePurgeInterval = 60 * 60 * 60 * 1000;
+const cachePath = path.join(process.cwd(), '.next/cache/fetch-cache');
+console.log(cachePath)
+const cachePurgeInterval = 60 * 1000;
 
 function purgeCache() {
     fs.rm(cachePath, { recursive: true }, (err) => {
-        const dateTime = new Date().toISOString();
         if (!err) {
-            console.log('[CACHE PURGATORY] Cache purged on : ', dateTime);
+            console.log('Cache purged');
         }
     });
 }
--- server/obfuscator.js
+++ server/obfuscator.js
@@ -1,33 +1,32 @@
-var JavaScriptObfuscator = require('javascript-obfuscator');
-const fs = require('fs');
+var JavaScriptObfuscator = require("javascript-obfuscator");
+const fs = require("fs");
 // create params like --local=true
 const params = process.argv.slice(2);
 // check if params --local=true
-const isLocal = params.includes('--local=true');
+const isLocal = params.includes("--local=true");
 // if local, skip obfuscation
 
-
-console.log('Obfuscating...');
+console.log("Obfuscating...");
 if (isLocal) {
-    console.log('Local mode detected, adding localhost to allowed host');
+  console.log("Local mode detected, adding localhost to allowed host");
 }
 // get list file with .js extension from ../.next/static complete with it's path
 const getFiles = (dir, files_) => {
-    files_ = files_ || [];
-    const files = fs.readdirSync(dir);
-    for (const i in files) {
-        const name = dir + '/' + files[i];
-        if (fs.statSync(name).isDirectory()) {
-            getFiles(name, files_);
-        } else if (name.endsWith('.js')) {
-            files_.push(name);
-        }
+  files_ = files_ || [];
+  const files = fs.readdirSync(dir);
+  for (const i in files) {
+    const name = dir + "/" + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_);
+    } else if (name.endsWith(".js")) {
+      files_.push(name);
     }
-    return files_;
-}
+  }
+  return files_;
+};
 
 // console.log all files
-const files = getFiles('./.next/static');
+const files = getFiles("./.next/static");
 // only include fils has /app/ in it's path
 // const filteredFiles = files.filter(file => file.includes('/app/'));
 const filteredFiles = files;
@@ -41,79 +40,83 @@ const seed = parseInt(`${phase1}${phase2}${phase3}`);
 console.log(`\n\n[!!] SEED : ${seed}\n\n`);
 
 // obfuscate all files
-filteredFiles.forEach(file => {
-    // console log obfuscating what file
-    // check if file is under 80kb
-    const stats = fs.statSync(file);
-    const fileSizeInBytes = stats.size;
-    const fileSizeInKilobytes = fileSizeInBytes / 1024;
-    // if file is over 80kb, skip obfuscation
-    if (fileSizeInKilobytes > 80) {
-        console.log("!! [SKIPPED] ", file);
-        return;
+filteredFiles.forEach((file) => {
+  // console log obfuscating what file
+  // check if file is under 80kb
+  const stats = fs.statSync(file);
+  const fileSizeInBytes = stats.size;
+  const fileSizeInKilobytes = fileSizeInBytes / 1024;
+  // if file is over 80kb, skip obfuscation
+  if (fileSizeInKilobytes > 80) {
+    console.log("!! [SKIPPED] ", file);
+    return;
+  }
+  console.log("[PROCESSING] ", file);
+
+  const obfuscationResult = JavaScriptObfuscator.obfuscate(
+    fs.readFileSync(file, "utf8"),
+    {
+      compact: true,
+      target: "browser",
+      seed: seed,
+      /** temp disable */
+      ...(isLocal
+        ? {}
+        : {
+            selfDefending: true,
+            debugProtection: true,
+            disableConsoleOutput: true,
+          }),
+
+      // ignoreImports: false,
+      domainLock: isLocal
+        ? ["http://localhost"]
+        : [
+            "https://bri-corpsite.dev-kjt.id",
+            "https://event-bri-corpsite.dev-kjt.id",
+            "http://event-bri-corpsite.dev-kjt.id",
+            "https://admin-event-bri.stg.service-kjt.id",
+            "https://event-bri.stg.service-kjt.id",
+            "https://event.bri.co.id",
+          ],
+      // domainLockRedirectUrl
+      sourceMap: false,
+
+      stringArray: true,
+      stringArrayRotate: true,
+      stringArrayShuffle: true,
+      stringArrayThreshold: 0.9,
+      stringArrayIndexShift: true,
+      stringArrayIndexesType: ["hexadecimal-number"],
+
+      stringArrayWrappersCount: 36,
+      stringArrayWrappersType: "variable",
+
+      stringArrayWrappersChainedCalls: true,
+      stringArrayEncoding: ["rc4"],
+
+      simplify: true,
+      transformObjectKeys: true,
+      numbersToExpressions: true,
+
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.55,
+
+      splitStrings: true,
+      splitStringsChunkLength: 8,
+
+      unicodeEscapeSequence: true,
+
+      /**
+       *
+       * DO NOT ENABLE THIS
+       * THIS WILL RENDER THE APP UNUSABLE
+       *
+       */
+      // identifierNamesGenerator: 'mangled',
+      // renameProperties: true,
+      // renamePropertiesMode: 'unsafe',
     }
-    console.log("[PROCESSING] ", file);
-
-    const obfuscationResult = JavaScriptObfuscator.obfuscate(
-        fs.readFileSync(file, 'utf8'),
-        {
-            compact: true,
-            target: 'browser',
-            seed: seed,
-            /** temp disable */
-            ...isLocal ? {} : {
-                selfDefending: true,
-                debugProtection: true,
-                disableConsoleOutput: true,
-            },
-
-            // ignoreImports: false,
-            domainLock: isLocal ? [
-                "http://localhost"
-            ] : [
-                "https://ppid.bri.co.id",
-                "https://ppid.dev.ddb.bri.co.id",
-                "https://bri-corpsite.dev-kjt.id",
-                "https://ppid.stg.ddb.bri.co.id",
-            ],
-            // domainLockRedirectUrl
-            sourceMap: false,
-
-            stringArray: true,
-            stringArrayRotate: true,
-            stringArrayShuffle: true,
-            stringArrayThreshold: 0.9,
-            stringArrayIndexShift: true,
-            stringArrayIndexesType: ['hexadecimal-number'],
-
-            stringArrayWrappersCount: 36,
-            stringArrayWrappersType: 'variable',
-
-            stringArrayWrappersChainedCalls: true,
-            stringArrayEncoding: ['rc4'],
-
-            simplify: true,
-            transformObjectKeys: true,
-            numbersToExpressions: true,
-
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 0.55,
-
-            splitStrings: true,
-            splitStringsChunkLength: 8,
-
-            unicodeEscapeSequence: true,
-
-            /**
-             * 
-             * DO NOT ENABLE THIS
-             * THIS WILL RENDER THE APP UNUSABLE
-             * 
-             */
-            // identifierNamesGenerator: 'mangled',
-            // renameProperties: true,
-            // renamePropertiesMode: 'unsafe',
-        }
-    );
-    fs.writeFileSync(file, obfuscationResult.getObfuscatedCode());
+  );
+  fs.writeFileSync(file, obfuscationResult.getObfuscatedCode());
 });
