777dc832c14952cabc9f643b9cb48a41fce46d66
ekyourkid <eky1579@gmail.com>
2025-02-25 15:30:29 +0700

    fix: two column layout in individu page

--- src/app/(views)/$element/portlet/client.portlet.variant04.tsx
+++ src/app/(views)/$element/portlet/client.portlet.variant04.tsx
@@ -17,57 +17,108 @@ const CE_PromoCard = ({
   imageUrl2,
 }: T_PromoCardProps) => {
   return (
-    <div className="relative flex md:flex-row flex-col my-10 container">
-      {description1 && (
-        <div className="flex-1 flex flex-col justify-center p-6 items-center z-10 md:order-1 order-2">
-          <div className="w-full flex">
-            <div
-              className={`mb-4 flex justify-center text-left w-full md:max-w-[650px]`}
-            >
-              {parseHTMLToReact(description1)}
+    <>
+      <div className="relative hidden lg:flex my-10">
+        {description1 && (
+          <div className="flex-1 flex flex-col justify-center p-6 items-center z-10">
+            <div className="w-full flex justify-end">
+              <div
+                className={`mb-4 flex justify-center text-center md:text-left w-full md:max-w-[650px]`}
+              >
+                {parseHTMLToReact(description1)}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {imageUrl1 && (
-        <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full md:order-2 order-1">
+        {imageUrl1 && (
           <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
-            <Image
-              src={imageUrl1}
-              alt={description1 ?? ''}
-              fill
-              className="object-cover w-full h-full"
-            />
-          </div>
-        </div>
-      )}
-
-      {imageUrl2 && (
-        <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full md:order-2 order-1">
-          <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
-            <Image
-              src={imageUrl2}
-              alt={description2 ?? ''}
-              fill
-              className="object-cover w-full h-full"
-            />
-          </div>
-        </div>
-      )}
-
-      {description2 && (
-        <div className="flex-1 flex flex-col justify-center p-6 items-center z-10 bg-white md:order-1 order-2">
-          <div className="w-full flex justify-start">
-            <div
-              className={`mb-4 flex justify-center text-center md:text-left w-full md:max-w-[650px]`}
-            >
-              {parseHTMLToReact(description2)}
+            <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+              <Image
+                src={imageUrl1}
+                alt={description1 ?? ''}
+                fill
+                className="object-cover w-full h-full"
+              />
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+        {imageUrl2 && (
+          <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+            <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+              <Image
+                src={imageUrl2}
+                alt={description2 ?? ''}
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        )}
+
+        {description2 && (
+          <div className="flex-1 flex flex-col justify-center p-6 items-center z-10 bg-white">
+            <div className="w-full flex justify-start">
+              <div
+                className={`mb-4 flex justify-center text-center md:text-left w-full md:max-w-[650px]`}
+              >
+                {parseHTMLToReact(description2)}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="relative flex flex-col lg:hidden my-10">
+        {description1 && (
+          <div className="flex-1 flex flex-col justify-center p-6 items-center z-10 md:order-1 order-2">
+            <div className="w-full flex justify-end">
+              <div
+                className={`mb-4 flex justify-center text-center w-full md:max-w-[650px]`}
+              >
+                {parseHTMLToReact(description1)}
+              </div>
+            </div>
+          </div>
+        )}
+
+        {imageUrl1 && (
+          <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+            <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+              <Image
+                src={imageUrl1}
+                alt={description1 ?? ''}
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        )}
+        {imageUrl2 && (
+          <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+            <div className="md:flex-1 relative md:h-[450px] h-[250px] w-full">
+              <Image
+                src={imageUrl2}
+                alt={description2 ?? ''}
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        )}
+
+        {description2 && (
+          <div className="flex-1 flex flex-col justify-center p-6 items-center z-10 bg-white">
+            <div className="w-full flex justify-start">
+              <div
+                className={`mb-4 flex justify-center text-center w-full md:max-w-[650px]`}
+              >
+                {parseHTMLToReact(description2)}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
