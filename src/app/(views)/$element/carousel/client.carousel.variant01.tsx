b8bf52997546ad7f17b58fd382a00cf603029786
Dede Rahmat F <108001133+Ryunehaimu@users.noreply.github.com>
2025-04-24 09:57:01 +0700

    image pada global header, carousel variant 11, simulation clear hardcode

--- src/app/(views)/$element/carousel/client.carousel.variant01.tsx
+++ src/app/(views)/$element/carousel/client.carousel.variant01.tsx
@@ -91,9 +91,9 @@ export function CE_CarouselVariant01({
               {data?.map((dataItem, index) => (
                 <div key={index} className="w-1/4 mdmax:w-1/2 flex-none px-2">
                   <Link href={handleurl(dataItem?.button?.link)} target="_self">
-                    <div className="p-4 mdmax:p-2 shadow-lg py-10 px-5">
+                    <div className="p-4 mdmax:p-2 shadow-lg py-10 px-5 h-[26rem] mdmax:h-[25rem] flex flex-col">
                       {dataItem?.image && (
-                        <div className="w-full h-[15rem] mb-4">
+                        <div className="w-full h-[15rem] mb-4 flex-shrink-0">
                           <Image
                             extern={false}
                             src={dataItem?.image ?? '/'}
@@ -104,7 +104,7 @@ export function CE_CarouselVariant01({
                           />
                         </div>
                       )}
-                      <div>
+                      <div className="flex flex-col flex-grow">
                         {dataItem?.title && (
                           <div className=" text-red-01 font-semibold mb-2">
                             {parseHTMLToReact(dataItem?.title)}
